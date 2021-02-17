36f9da3629f9e29c257161452890454b744fc364
Sam Belliveau <sam.belliveau@gmail.com>
2021-02-17 15:58:26 -0500

    removed unused parts of branch

--- src/main/java/com/stuypulse/robot/Constants.java
+++ src/main/java/com/stuypulse/robot/Constants.java
@@ -22,80 +22,13 @@ import edu.wpi.first.wpilibj.kinematics.DifferentialDriveKinematics;
  */
 public interface Constants {
 
-    // This is the scale for units, this is used to make the robot simulate another
-    // size
-    // The romi is about 5 inches across, and edwin is about 30 inches across
-    // By setting the robot scale to 1.0 / 5.0, we can simulate a robot the size of
-    // edwin
-    double ROBOT_SCALE = 1.0 / 5.0;
-
-    // for file io
-    Path DEPLOY_DIRECTORY = Filesystem.getDeployDirectory().toPath();
-
-    interface OnBoardIO {
-        double MESSAGE_INTERVAL = 1.0;
-    }
-
     interface Drivetrain {
 
-        SmartBoolean USE_GYROSCOPE = new SmartBoolean("Use Gyroscope", false);
-
-        int SENDABLE_SIGFIGS = 5;
-
-        double LEFT_VOLTAGE_MUL = 1.0;
-        double RIGHT_VOLTAGE_MUL = -1.0;
-
-        double TRACK_WIDTH = 0.141 / ROBOT_SCALE;
-        double DEAD_BAND = 0.05;
-
         interface Ports {
+            
             int LEFT_MOTOR = 0;
             int RIGHT_MOTOR = 1;
 
-            int LEFT_ENCODER_A = 4;
-            int LEFT_ENCODER_B = 5;
-
-            int RIGHT_ENCODER_A = 6;
-            int RIGHT_ENCODER_B = 7;
-        }
-
-        interface Odometry {
-            double START_X = 0.0;
-            double START_Y = 0.0;
-
-            Rotation2d START_ANG = new Rotation2d(0.0);
-
-            Pose2d START = new Pose2d(START_X, START_Y, START_ANG);
-        }
-
-        interface Encoders {
-            double COUNTS_PER_REVOLUTION = 1440.0;
-            double WHEEL_DIAMETER_METER = 0.070 / ROBOT_SCALE;
-
-            double DISTANCE_PER_PULSE = (Math.PI * WHEEL_DIAMETER_METER) / COUNTS_PER_REVOLUTION;
-        }
-
-        interface Motion {
-
-            DifferentialDriveKinematics KINEMATICS = new DifferentialDriveKinematics(TRACK_WIDTH);
-
-            SimpleMotorFeedforward MOTOR_FEED_FORWARD = new SimpleMotorFeedforward(
-                FeedForward.S,
-                FeedForward.V, 
-                FeedForward.A
-            );
-
-            interface FeedForward {
-                double S = 0.376;
-                double V = 10.00 * ROBOT_SCALE;
-                double A = 0.186 * ROBOT_SCALE;
-            }
-
-            interface PID {
-                double P = 0.0125 * ROBOT_SCALE;
-                double I = 0 * ROBOT_SCALE;
-                double D = 0 * ROBOT_SCALE;
-            }
         }
 
     }
