e6991407b7736a86dbf7574cb64a78fbd43ace72
Sam Belliveau <sam.belliveau@gmail.com>
2021-02-18 13:50:04 -0500

    setup lesson

--- src/main/java/com/stuypulse/robot/commands/DrivetrainDriveCommand.java
+++ src/main/java/com/stuypulse/robot/commands/DrivetrainDriveCommand.java
@@ -1,8 +1,13 @@
 package com.stuypulse.robot.commands;
 
-import com.stuypulse.robot.subsystems.Drivetrain;
 import com.stuypulse.stuylib.input.Gamepad;
+import com.stuypulse.robot.subsystems.Drivetrain;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+
+// TODO: Find out what we can use from here
+//      - IFilter: https://stuypulse.github.io/StuyLib/com/stuypulse/stuylib/streams/filters/IFilter.html
+//      - All of the filters: https://stuypulse.github.io/StuyLib/com/stuypulse/stuylib/streams/filters/package-summary.html
+import com.stuypulse.stuylib.streams.filters.*;
 
 public class DrivetrainDriveCommand extends CommandBase {
     
@@ -23,7 +28,13 @@ public class DrivetrainDriveCommand extends CommandBase {
      
     @Override
     public void execute() {
-        drivetrain.arcadeDrive(gamepad.getRightTrigger() - gamepad.getLeftTrigger(), gamepad.getLeftStick().x);
+
+        double speed = gamepad.getRightTrigger() - gamepad.getLeftTrigger();
+        double turn = gamepad.getLeftStick().x;
+
+        // TODO: Filter these values sending them to the drivetrain
+
+        drivetrain.arcadeDrive(speed, turn);
 
     }
     
