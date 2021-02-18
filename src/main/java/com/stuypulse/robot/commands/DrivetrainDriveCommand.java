5f7b5928c8d70087438ea62fa9768938ccb4d558
Anthony Chen <50970130+ACPlayGames@users.noreply.github.com>
2021-02-18 17:20:12 -0500

    Add Group 1 filters to DrivetrainDriveCommand
    Co-authored-by: Myles Pasetsky <mp3blue99@gmail.com>

--- src/main/java/com/stuypulse/robot/commands/DrivetrainDriveCommand.java
+++ src/main/java/com/stuypulse/robot/commands/DrivetrainDriveCommand.java
@@ -10,17 +10,32 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 import com.stuypulse.stuylib.streams.filters.*;
 
 public class DrivetrainDriveCommand extends CommandBase {
-    
+
     private final Drivetrain drivetrain; 
     private final Gamepad gamepad;
-    
+
+    private final IFilter filter = new IFilterGroup (
+        (x) -> SLMath.square(x),
+        new LowPassFilter(0.7),
+        new MovingAverage(10)
+    );
+
     public DrivetrainDriveCommand(Drivetrain drivetrain, Gamepad gamepad) {
         this.drivetrain = drivetrain;
         this.gamepad = gamepad;
         
         addRequirements(drivetrain);
     }
-     
+
+    @Override
+    public void initalize() {
+        filter = new IFilterGroup (
+            (x) -> SLMath.square(x),
+            new LowPassFilter(0.7),
+            new MovingAverage(10)
+        );
+    }
+
     @Override
     public void execute() {
 
@@ -29,7 +44,6 @@ public class DrivetrainDriveCommand extends CommandBase {
 
         // TODO: Filter these values sending them to the drivetrain
 
-        drivetrain.arcadeDrive(speed, turn);
-
+        drivetrain.arcadeDrive(filter.get(speed), filter.get(turn));
     }
 }
